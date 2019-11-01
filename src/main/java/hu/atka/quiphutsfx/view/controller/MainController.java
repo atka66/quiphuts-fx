191d4acd1845a5965bb378e48cde8f4ee8681620
atka66 <atkavagyok@gmail.com>
2019-11-01 10:26:56 +0100

    stub added

---
+++ src/main/java/hu/atka/quiphutsfx/view/controller/MainController.java
@@ -0,0 +1,11 @@
+package hu.atka.quiphutsfx.view.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable {
+
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+}
---
+++ src/main/java/hu/atka/quiphutsfx/view/resources/Settings.java
@@ -0,0 +1,6 @@
+package hu.atka.quiphutsfx.view.resources;
+
+public class Settings {
+	public static final int WINDOW_WIDTH = 640;
+	public static final int WINDOW_HEIGHT = 480;
+}
